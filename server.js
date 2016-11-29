03b5d31fde407fa8addfc9429987c9317f8b8f02
tdkovaci <tdkovaci@purdue.edu>
2016-11-29 16:18:15 -0500

    Changed how players are structured
    Super class structure with entity

--- server.js
+++ server.js
@@ -45,10 +45,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
-
-var SOCKET_LIST = {};
-var PLAYER_LIST = {};
+//var PLAYER_LIST = {};
 
 // List of rooms 
 var ROOM_LIST = ["room1"];
@@ -59,38 +56,119 @@ var queue = [];
 //Number of connections
 var numOfConnections = 0;
 
+// General entity super class.
+var entity = function(){
+    var self = {
+      x:Math.floor(Math.random() * 250) + 30,
+      y:Math.floor(Math.random() * 250) + 30,
+      spdX:0,
+      spdY:0,
+      id:"",
+    }
+
+    // Updates position: x and y coords
+    self.updatePosition = function(){
+      self.x += self.spdX;
+      self.y += self.spdY;
+    }
+
+    // General update method that is called from outside. Can put other important things in here
+    // ex: collision update
+    self.update = function(){
+      self.updatePosition();
+    }
+
+    return self;
+}
+
 var Player = function(id){
-  // Basically a constructor
-  var self = {
-    x:Math.floor(Math.random() * 250) + 30,
-    y:Math.floor(Math.random() * 250) + 30,
-    id:id,
-    room:"no room",
-    number:"" + Math.floor(10 * Math.random()),
-    pressingRight:false,
-    pressingLeft:false,
-    pressingUp:false,
-    pressingDown:false,
-    maxSpeed:10,
+  var self = entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.maxSpeed = 10;
+  self.room = "no room";
+
+  // overwrite super update by including updateSpd
+  var super_update = self.update;
+  self.update = function(){
+    self.updateSpd();
+    super_update();
   }
 
-  // On certain inputs, increases or decreases position
-  self.updatePos = function(){
+  // Handle keyboard inputs
+  self.updateSpd = function(){
+    // right and left
     if(self.pressingRight){
-      self.x += self.maxSpeed;
+      self.spdX = self.maxSpeed;
+    } else if(self.pressingLeft){
+      self.spdX = -self.maxSpeed;
+    } else {
+      self.spdX = 0;
     }
-    if(self.pressingLeft){
-      self.x -= self.maxSpeed;
-    }
+
+    // up and down
     if(self.pressingUp){
-      self.y -= self.maxSpeed;
-    } 
-    if(self.pressingDown){
-      self.y += self.maxSpeed;
+      self.spdY = -self.maxSpeed;
+    } else if(self.pressingDown){
+      self.spdY = self.maxSpeed;
+    } else {
+      self.spdY = 0;
+    }
+
+  }
+
+  Player.list[id] = self;
+  return self;
+}
+
+// Static list of players
+Player.list = {};
+
+// Handles player specific stuff
+Player.onConnect = function(socket){
+  var player = Player.list[socket.id];
+  // Listens for client 'keyPress'
+  // Updates the player position on server
+  socket.on('keyPress', function(data){
+    if(data.inputId === 'left'){
+      player.pressingLeft = data.state;
+    } else if(data.inputId === 'right'){
+      player.pressingRight = data.state;
+    } else if(data.inputId === 'up'){
+      player.pressingUp = data.state;
+    } else if(data.inputId === 'down'){
+      player.pressingDown = data.state;
+    }
+
+  });
+}
+
+// Static onDisconnect function for all Players
+Player.onDisconnect = function(socket){
+  delete Player.list[socket.id];
+}
+
+// Static update function for all Players
+Player.update = function(players){
+  var dataPackage = [];
+  // Loop through each player in given player array
+  for(var i in players){
+    var player = players[i];
+    if(player != undefined){
+      player.update();
+      dataPackage.push({
+        x:player.x,
+        y:player.y,
+        number:player.number
+      });
     }
   }
 
-  return self;
+  return dataPackage;
 }
 
 var io = require('socket.io')(server, {});
@@ -102,7 +180,7 @@ io.sockets.on('connection', function(socket){
   SOCKET_LIST[socket.id] = socket;
 
   var player = Player(socket.id);
-  PLAYER_LIST[socket.id]=player;
+  //PLAYER_LIST[socket.id]=player;
 
   queue.push(player);
   console.log("Queue size after connection: " + queue.length);
@@ -110,6 +188,86 @@ io.sockets.on('connection', function(socket){
   // Take two players out of the queue, add them to a room
   if(queue.length>=2){
 
+    handleRoom(queue);
+    
+  }
+
+  Player.onConnect(socket);
+
+  // When a player disconnects, remove them from the socket list
+  socket.on('disconnect', function(){
+    // Get room that the socket was connected to
+    var temp = Player.list[socket.id];
+    var room = temp.room;
+
+    // Remove the socket from the server and player list
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+
+    // Force socket to leave the room it was connected to
+    socket.leave(room);
+
+    // Remove it from the queue if it was in it
+    var index = queue.indexOf(socket.id);
+    if(index >=0 ){
+      queue.splice(index,1);
+    }
+    
+    numOfConnections--;
+  });
+
+  
+
+});
+
+// Do stuff in function, every x amount of time
+setInterval(function(){
+  // Loop through all rooms
+  for(var i in ROOM_LIST){
+
+    // Create empty data package
+    var dataPackage = [];
+
+    //Get all clients in a room
+    room = ROOM_LIST[i];
+
+    // If the room does not exist, stop
+    if(io.sockets.adapter.rooms[room] == undefined){
+      break;
+    }
+
+    // Get all clients inside the room
+    var socketIDs = [];
+    for (var socketId in io.sockets.adapter.rooms[room].sockets) {
+      socketIDs.push(socketId);
+    }
+    
+    //If there are people actually in the room
+    if(socketIDs.length>0){
+
+      // Create list of players based off of the socket IDS
+      var players = [];
+      players.push(Player.list[socketIDs[0]]);
+      players.push(Player.list[socketIDs[1]]);
+
+      // Get updated data from the players
+      dataPackage = Player.update(players);
+      
+      // Send the data to the respective players
+      for(var i in players){
+        if(players[i] != undefined){
+          var socket = SOCKET_LIST[players[i].id];
+            socket.emit('newPosition', dataPackage);
+        }
+      }
+
+    }
+
+  }
+}, 1000/25);
+
+// Handles room separation and queue stuff
+handleRoom = function(queue){
     var player1 = queue.shift();
     var player2 = queue.shift();
 
@@ -173,84 +331,4 @@ io.sockets.on('connection', function(socket){
     p1Socket.join(roomToJoin);
     p2Socket.join(roomToJoin);
     console.log("Joined room: " + roomToJoin);
-  }
-
-  // When a player disconnects, remove them from the socket list
-  socket.on('disconnect', function(){
-    var temp = PLAYER_LIST[socket.id];
-    var room = temp.room;
-
-    delete SOCKET_LIST[socket.id];
-    delete PLAYER_LIST[socket.id];
-    socket.leave(room);
-    var index = queue.indexOf(socket.id);
-    queue.splice(index,1);
-    numOfConnections--;
-  });
-
-  // Listens for client 'keyPress'
-  // Updates the player position on server
-  socket.on('keyPress', function(data){
-    if(data.inputId === 'left'){
-      player.pressingLeft = data.state;
-    } else if(data.inputId === 'right'){
-      player.pressingRight = data.state;
-    } else if(data.inputId === 'up'){
-      player.pressingUp = data.state;
-    } else if(data.inputId === 'down'){
-      player.pressingDown = data.state;
-    }
-
-  });
-
-});
-
-// Do stuff in function, every x amount of time
-setInterval(function(){
-  for(var i in ROOM_LIST){
-
-    var dataPackage = [];
-
-    //Get all clients in a room
-    room = ROOM_LIST[i];
-
-    if(io.sockets.adapter.rooms[room] == undefined){
-      break;
-    }
-
-    var socketIDs = [];
-    for (var socketId in io.sockets.adapter.rooms[room].sockets) {
-      socketIDs.push(socketId);
-    }
-    
-      //If there are people actually in the room
-      if(socketIDs.length>0){
-
-        // Create list of players based off of the socket IDS
-        var players = [];
-        players.push(PLAYER_LIST[socketIDs[0]]);
-        players.push(PLAYER_LIST[socketIDs[1]]);
-
-        // Loop through each client
-        for(var i in players){
-          var player = players[i];
-          if(player != undefined){
-            player.updatePos();
-            dataPackage.push({
-              x:player.x,
-              y:player.y,
-              number:player.number
-            });
-          }
-        }
-        
-        for(var i in players){
-          if(players[i] != undefined){
-            var socket = SOCKET_LIST[players[i].id];
-              socket.emit('newPosition', dataPackage);
-          }
-        }
-    }
-
-  }
-}, 1000/25);
+}
